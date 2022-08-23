ec120064e9517feeb85f752329150a2a67e00105
torone89 <torone89>
2022-08-23 12:56:41 +0200

    Adding BaseHeader/ Base Footer / TheMain component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <base-header />
+    <the-main />
+
+    <base-footer />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import BaseHeader from "./components/BaseHeader.vue";
+import TheMain from "./components/TheMain.vue";
+import BaseFooter from "./components/BaseFooter.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    BaseHeader,
+    TheMain,
+    BaseFooter,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/BaseFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>prova</div>
+</template>
+
+<script>
+export default {
+  name: "BaseFooter",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
